091783c4b265ea9948f4e524af5c7289f4228e71
elisabasso00 <elisa.ba2000@gmail.com>
2023-06-15 19:57:44 +0200

    Update process.py

--- process.py
+++ process.py
@@ -2,14 +2,25 @@ import pyspark
 from pyspark import SparkContext
 from pyspark.sql import SparkSession 
 from pyspark.sql import SQLContext
-import os
 
-#os.environ['SPARK_HOME'] = ''
-os.environ["JAVA_HOME"] = "C:\Program Files (x86)\Java\jre-1.8"
+connection_string="mongodb+srv://admin:psw@cluster0.ew7zhpy.mongodb.net"
+
+spark = SparkSession.builder.appName("MongoDBSparkConnector") \
+    .config("spark.mongodb.input.uri", connection_string) \
+    .config("spark.mongodb.output.uri", connection_string) \
+    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.1.1") \
+    .getOrCreate()
+
+sc = spark.sparkContext.setLogLevel("WARN")
+
+df = spark.read.format("mongodb").option("uri", connection_string).option("database", "mydatabase").option("collection", "customers").load()
+df.printSchema()
+spark.stop()
+
+# spark.conf.set("spark.mongodb.input.connectionTimeoutMS", "100000")  # Set timeout to 100 seconds
 
 
-conf=pyspark.SparkConf().set("spark.jars.packages", "org.mongodb.spark:mongo=spark=connector_2.12:3.0.1"
-                             ).setMaster("local").setAppName("MyApp").setAll([("spark.driver.memory", "40g"), 
-                                                                              ("spark.executor.memory","50g")])
-
-sc=SparkContext(conf=conf)
+# conf=pyspark.SparkConf().set("spark.jars.packages", "mongodb+srv://admin:psw@cluster0.ew7zhpy.mongodb.net/"
+#                              ).setMaster("").setAppName("MyApp").setAll([("spark.driver.memory", "40g"), 
+#                                                                               ("spark.executor.memory","50g")])
+# sc=SparkContext(conf=conf)
