0174ab15d95c69332d9073c1ea39ae7f0cbdb0e2
sraatgn <sarategoni18@gmail.com>
2023-06-17 11:40:51 +0200

    Update api_requests.py
    - Changed air quality API to weatherbit (retrieves also historical AQI)
    - added request_time to each document

--- api_sink/api_requests.py
+++ api_sink/api_requests.py
@@ -9,6 +9,8 @@ import yaml
 # REAL TIME REQUESTS -------------------------------------
 def rt_tomtom_request(lat, long_):
 
+    """ retrieves current traffic-flow """
+
     url = f"https://api.tomtom.com/traffic/services/4/flowSegmentData/relative0/10/json?point={lat}%2C{long_}&unit=KMPH&openLr=false&key=FShxCHa2f5LlOL479fZRSYJJuTGBe3J4"
 
     payload = {}
@@ -16,32 +18,53 @@ def rt_tomtom_request(lat, long_):
 
     response = requests.request("GET", url, headers=headers, data=payload)
     data_json = response.json()
-    result = data_json["flowSegmentData"]
+    time = datetime.datetime.now().isoformat()
+
+    result = {
+        'request_time': time,
+        'request_data': data_json['flowSegmentData']
+    }
     
     return result 
 
 
 def rt_air_request(lat, long_):
-    url = f"http://api.airvisual.com/v2/nearest_city?lat={lat}&lon={long_}&key=a2abc955-cedb-4d19-ab75-1d3346eee4b6&=fbfab6a9659cda24ae3aa9d35cba5d070307c29e"
+
+    """ retrieves hourly AQI for current hour & past 72hrs """
+
+    url = f"https://api.weatherbit.io/v2.0/history/airquality?lat={lat}&lon={long_}&key=fd7a60de53d94247a0a16cfdec16d636"
 
     payload = {}
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload)
     data_json = response.json()
-    result = data_json["data"]
+    time = datetime.datetime.now().isoformat()
+
+    result = {
+        'request_time': time,
+        'request_data': data_json['data']
+    }
 
     return result
 
 
 def rt_weather_request(id_localita, days:int, language='en'):
 
+    """ retrieves hourly weather forecast for next 96hrs """
+
     url = f"https://api.3bmeteo.com/publicv3/bollettino_meteo/previsioni_localita/{id_localita}/{days}/{language}/hourly/1?format=json2&X-API-KEY=xyCfMl5omzOMALMITS0oDkHJBptTXomH0tAbOleH"
     payload = {}
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload)
-    result = response.json()
+    data_json = response.json()
+    time = datetime.datetime.now().isoformat()
+
+    result = {
+        'request_time': time,
+        'request_data': data_json
+    }
     
     return result
 
@@ -65,11 +88,9 @@ def insert_docs(tomtom_data, air_data, weather_data):
     air_id = mydb.air.insert_one(air_data).inserted_id
     weather_id = mydb.weather.insert_one(weather_data).inserted_id
 
-    time = datetime.datetime.now().isoformat()
+    print(f"You just inserted: {traffic_id} & {air_id} & {weather_id}")
 
-    print(f"You just inserted: {traffic_id} & {air_id} & {weather_id}\nTime: {time}")
-
-    return traffic_id, air_id, weather_id, time
+    return traffic_id, air_id, weather_id
 
 # REQUEST PARAMETERS (just for now here)------------------
 if __name__ == "__main__":
@@ -87,5 +108,6 @@ if __name__ == "__main__":
     trento_id = 7428
 
     tomtom_data, air_data, weather_data = get_all_requests(in_lat, in_long, trento_id, 4)
-    insert_docs(tomtom_data, air_data, weather_data)
+    traffic_id, air_id, weather_id = insert_docs(tomtom_data, air_data, weather_data)
+    
 
