5c64d1e28e2762b4232c1f98628689c8da8899aa
sraatgn <sarategoni18@gmail.com>
2023-06-20 23:50:31 +0200

    Spark is now working
    + added connection string for mongo local host to config.yaml

--- tail/process.py
+++ tail/process.py
@@ -2,25 +2,44 @@ import pyspark
 from pyspark import SparkContext
 from pyspark.sql import SparkSession 
 from pyspark.sql import SQLContext
+from pyspark.sql.types import StructType  # to write data schema
 
-connection_string="mongodb+srv://admin:psw@cluster0.ew7zhpy.mongodb.net/"
+connection_string="mongodb+srv://admin:psw@cluster0.ew7zhpy.mongodb.net/"    # old atlas connection 
+#connection_string = "mongodb://localhost:27017/"
 
-spark = SparkSession.builder.appName("MongoDBSparkConnector") \
-    .config("spark.mongodb.input.uri", connection_string) \
-    .config("spark.mongodb.output.uri", connection_string) \
+spark = SparkSession.builder.master("local").appName("MongoDBSparkConnector") \
+    .config("spark.driver.memory", "15g") \
+    .config("spark.mongodb.read.connection.uri", connection_string) \
+    .config("spark.mongodb.write.connection.uri", connection_string) \
     .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.1.1") \
     .getOrCreate()
 
 sc = spark.sparkContext.setLogLevel("WARN")
 
-df = spark.read.format("mongodb").option("uri", connection_string).option("database", "mydatabase").option("collection", "customers").load()
+## FOLLOWING SECTION IS TO FIX
+# read and create spark dataframe
+df = spark.read.format("mongodb") \
+    .option("uri", connection_string) \
+    .option("database", "mydatabase") \
+    .option("collection", "tomtom") \
+    .option("collection", "air") \
+    .option("collection", "weather") \
+    .load()
+
 df.printSchema()
+
+# Create a schema for the JSON data #TO CHECK WITH df.printSchema above with actual database
+json_schema = StructType().add("request_time", "string") \
+                          .add("traffic_flow", "double") \
+                          .add("aqi", "integer") \
+                          .add("precipitazioni", "integer") \
+                          .add("prob_precipitazioni", "integer") \
+                          .add("wind", "double")    # maybe put as double ?
+
+# Convert JSON data to a PySpark DataFrame
+#data_df = spark.createDataFrame(df, schema=json_schema)
+df.show()
+
 spark.stop()
 
-# spark.conf.set("spark.mongodb.input.connectionTimeoutMS", "100000")  # Set timeout to 100 seconds
 
-
-# conf=pyspark.SparkConf().set("spark.jars.packages", "mongodb+srv://admin:psw@cluster0.ew7zhpy.mongodb.net/"
-#                              ).setMaster("").setAppName("MyApp").setAll([("spark.driver.memory", "40g"), 
-#                                                                               ("spark.executor.memory","50g")])
-# sc=SparkContext(conf=conf) 
