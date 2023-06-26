b250a776637bf06f17856169ac27c0b81708e156
sraatgn <128134237+sraatgn@users.noreply.github.com>
2023-06-26 15:23:39 +0200

    Update spark_process.py
    Added few explanations for future development.

--- tail/spark_process.py
+++ tail/spark_process.py
@@ -10,6 +10,9 @@ logger = logging.getLogger('py4j')
 logger.setLevel(logging.ERROR)
 print("\tSpark on.")
 
+## PARSE PREPROCESSED DATA
+# ... code in tail_manager.py with mongoDB connection & pre-processing stage returning dictionaries ...
+
 ## CORE COMPUTATIONS (with Spark)
 rdd_weather = spark.sparkContext.parallelize(pp_weather["forecasts"])
 rdd_traffic = spark.sparkContext.parallelize(pp_traffic["forecasts"])
@@ -20,8 +23,10 @@ rdd1_formatted = rdd_weather.map(lambda x: (x[0], x))
 rdd2_formatted = rdd_traffic.map(lambda x: (x[0], x))   
 
 rdd_joined = rdd1_formatted.join(rdd2_formatted)
-
-output_rdd = rdd_joined.map(lambda data_point: aqi_formula(data_point[0][2],   # traffic
+# map aqi_formula on each element of the joined rdd
+output_rdd = rdd_joined.map(lambda data_point: aqi_formula(data_point[0][2],    # traffic
                                                         data_point[1][1],       # prec
                                                         data_point[1][2],       # prob_prec
                                                         data_point[1][3]))      # wind
+# extract output AQI predictions
+predictions = output_rdd.collect()
